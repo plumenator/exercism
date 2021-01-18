7af9ef2349bde747eca97848877b01e188248782
Karthik Ravikanti <karthik.ravikanti@gmail.com>
2021-01-18 23:48:11 +0800

    Avoid superfluous evaluation of the third condition

--- haskell/leap/src/LeapYear.hs
+++ haskell/leap/src/LeapYear.hs
@@ -2,6 +2,7 @@ module LeapYear (isLeapYear) where
 
 isLeapYear :: Integer -> Bool
 isLeapYear year = 4 `divides` year
-                  && not (100 `divides` year)
-                  || 400 `divides` year where
-  divides x y = y `rem` x == 0
+                  && (not (100 `divides` year)
+                      || 400 `divides` year)
+  where
+    divides x y = y `rem` x == 0
