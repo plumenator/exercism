32a6b415ab8cf2b9a041fa0365b2dcd86cb35919
Karthik Ravikanti <karthik.ravikanti@gmail.com>
2021-01-18 17:47:39 +0800

    Reformat as suggested by clippy

--- rust/gigasecond/src/lib.rs
+++ rust/gigasecond/src/lib.rs
@@ -2,5 +2,5 @@ use chrono::{DateTime, Utc};
 
 // Returns a Utc DateTime one billion seconds after start.
 pub fn after(start: DateTime<Utc>) -> DateTime<Utc> {
-    start + chrono::Duration::seconds(1000_000_000)
+    start + chrono::Duration::seconds(1_000_000_000)
 }
