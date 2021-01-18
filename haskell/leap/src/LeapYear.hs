7bb96acb2d6298bf9ef99c50d2efdd49e0f9d9d6
Karthik Ravikanti <karthik.ravikanti@gmail.com>
2021-01-18 18:00:51 +0800

    Optimize further

--- haskell/leap/src/LeapYear.hs
+++ haskell/leap/src/LeapYear.hs
@@ -3,6 +3,6 @@ module LeapYear (isLeapYear) where
 isLeapYear :: Integer -> Bool
 isLeapYear year
   | year `rem` 4 /= 0 = False
-  | year `rem` 400 == 0 = True
-  | year `rem` 100 == 0 = False
+  | year `rem` 100 /= 0 = True
+  | year `rem` 400 /= 0 = False
   | otherwise = True
