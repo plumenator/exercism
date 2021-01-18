9fc60b06ad3ca325db24d0222bb969f8f312ae91
Karthik Ravikanti <karthik.ravikanti@gmail.com>
2021-01-18 17:02:03 +0800

    Solve bonus (grapheme clusters)

--- rust/reverse-string/src/lib.rs
+++ rust/reverse-string/src/lib.rs
@@ -1,3 +1,13 @@
+#[cfg(feature = "grapheme")]
+use unicode_segmentation::UnicodeSegmentation;
+
 pub fn reverse(input: &str) -> String {
-    input.chars().rev().collect()
+    #[cfg(feature = "grapheme")]
+    {
+        input.graphemes(/* is_extended */ true).rev().collect()
+    }
+    #[cfg(not(feature = "grapheme"))]
+    {
+        input.chars().rev().collect()
+    }
 }
