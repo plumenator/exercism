9f7af9c3339c1b8de98962216123282ea081fc08
Karthik Ravikanti <karthik.ravikanti@gmail.com>
2021-01-19 19:30:17 +0800

    Store only minutes
    This simplifies Clock::add_minutes() and also separates the
    hour/minute splitting.
    
    Thanks, Bob Hoeppner!

--- rust/clock/src/lib.rs
+++ rust/clock/src/lib.rs
@@ -1,29 +1,29 @@
 #[derive(PartialEq, Eq, Debug)]
 pub struct Clock {
-    hours: i32,
     minutes: i32,
 }
 
 impl Clock {
     pub fn new(hours: i32, minutes: i32) -> Self {
-        Self {
-            hours: 0,
-            minutes: 0,
-        }
-        .add_minutes(hours * 60 + minutes)
+        Self { minutes: 0 }.add_minutes(hours * 60 + minutes)
     }
 
     pub fn add_minutes(&self, excess_minutes: i32) -> Self {
         let negmod = |x, l| x % l + if x < 0 { l } else { 0 };
-        let total_minutes = negmod(self.hours * 60 + self.minutes + excess_minutes, 24 * 60);
-        let hours = total_minutes / 60;
-        let minutes = total_minutes - hours * 60;
-        Self { hours, minutes }
+        let minutes = negmod(self.minutes + excess_minutes, 24 * 60);
+        Self { minutes }
+    }
+
+    fn hours_and_minutes(&self) -> (i32, i32) {
+        let hours = self.minutes / 60;
+        let minutes = self.minutes - hours * 60;
+        (hours, minutes)
     }
 }
 
 impl std::fmt::Display for Clock {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, "{:02}:{:02}", self.hours, self.minutes)
+        let (hours, minutes) = self.hours_and_minutes();
+        write!(f, "{:02}:{:02}", hours, minutes)
     }
 }
