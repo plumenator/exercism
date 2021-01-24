53e7eb7032243747e637e82054c1e32f3351279d
Karthik Ravikanti <karthik.ravikanti@gmail.com>
2021-01-24 21:18:51 +0800

    Use iterate'
    According to the mentor, laziness doesn't help here.

--- haskell/collatz-conjecture/src/CollatzConjecture.hs
+++ haskell/collatz-conjecture/src/CollatzConjecture.hs
@@ -1,9 +1,11 @@
 module CollatzConjecture (collatz) where
 
+import Data.List (iterate')
+
 collatz :: Integer -> Maybe Integer
 collatz n | n <= 0 = Nothing
 collatz n = Just collatz' where
-  collatz' = countUntil (== 1) $ iterate step n
+  collatz' = countUntil (== 1) $ iterate' step n
   step x | even x = x `div` 2
          | otherwise = 3 * x + 1
   countUntil p = toInteger . length . takeWhile (not . p)
