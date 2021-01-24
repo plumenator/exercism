d6f9be53dcf3b28c17b83a917aeb759727d0af81
Karthik Ravikanti <karthik.ravikanti@gmail.com>
2021-01-24 20:23:32 +0800

    Use iterate instead of scanl, flip, $, and repeat

--- haskell/collatz-conjecture/src/CollatzConjecture.hs
+++ haskell/collatz-conjecture/src/CollatzConjecture.hs
@@ -3,7 +3,7 @@ module CollatzConjecture (collatz) where
 collatz :: Integer -> Maybe Integer
 collatz n | n <= 0 = Nothing
 collatz n = Just collatz' where
-  collatz' = len . takeWhile (/= 1) $ scanl (flip ($)) n (repeat step)
+  collatz' = len . takeWhile (/= 1) $ iterate step n
   step x | even x = x `div` 2
          | otherwise = 3 * x + 1
   len = sum . map (const 1)
