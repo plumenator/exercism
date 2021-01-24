2ca21d1b657067ab0da77fd20afe394446d4503b
Karthik Ravikanti <karthik.ravikanti@gmail.com>
2021-01-24 21:29:48 +0800

    Use quot
    `quot`, just like `rem` is more performant

--- haskell/collatz-conjecture/src/CollatzConjecture.hs
+++ haskell/collatz-conjecture/src/CollatzConjecture.hs
@@ -6,6 +6,6 @@ collatz :: Integer -> Maybe Integer
 collatz n | n <= 0 = Nothing
 collatz n = Just collatz' where
   collatz' = countUntil (== 1) $ iterate' step n
-  step x | even x = x `div` 2
+  step x | even x = x `quot` 2
          | otherwise = 3 * x + 1
   countUntil p = genericLength . takeWhile (not . p)
