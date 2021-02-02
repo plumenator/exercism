bde983d42aca36b2b38317e30c669d58a98933dd
Karthik Ravikanti <karthik.ravikanti@gmail.com>
2021-02-02 13:12:41 +0800

    Use elemIndex
    Rids us of countUntil, but forces us to use fromIntegral and lose precision

--- haskell/collatz-conjecture/src/CollatzConjecture.hs
+++ haskell/collatz-conjecture/src/CollatzConjecture.hs
@@ -1,11 +1,9 @@
 module CollatzConjecture (collatz) where
 
-import Data.List (genericLength, iterate')
+import Data.List (elemIndex, iterate')
 
 collatz :: Integer -> Maybe Integer
 collatz n | n <= 0 = Nothing
-collatz n = Just collatz' where
-  collatz' = countUntil (== 1) $ iterate' step n
+collatz n = fmap fromIntegral . elemIndex 1 $ iterate' step n where
   step x  = if r == 0 then q else 3 * x + 1 where
     (q, r) = quotRem x 2
-  countUntil p = genericLength . takeWhile (not . p)
