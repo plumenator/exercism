7cd9a58914a96a46d31071e348028f8bf249553f
Karthik Ravikanti <karthik.ravikanti@gmail.com>
2021-02-02 16:04:51 +0800

    Use mapM

--- haskell/rna-transcription/src/DNA.hs
+++ haskell/rna-transcription/src/DNA.hs
@@ -1,10 +1,7 @@
 module DNA (toRNA) where
 
 toRNA :: String -> Either Char String
-toRNA = foldr cons (Right []) . map toComplement where
-  cons (Left c)  _ = Left c
-  cons _ (Left c) = Left c
-  cons (Right c) (Right cs) = Right (c : cs)
+toRNA =  mapM toComplement
 
 toComplement :: Char -> Either Char Char
 toComplement 'G' = Right 'C'
