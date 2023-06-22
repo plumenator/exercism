a571ac8dda1447bde46c921ee8ca03a4a3c2e484
Karthik Ravikanti <karthik.ravikanti@gmail.com>
2023-06-22 15:25:37 +0800

    Solve nim/leap without if

--- nim/leap/leap.nim
+++ nim/leap/leap.nim
@@ -1,9 +1,4 @@
 proc isLeapYear*(year: int): bool =
-  if year mod 400 == 0:
-    true
-  elif year mod 100 == 0:
-    false
-  elif year mod 4 == 0:
-    true
-  else:
-    false
+  year mod 400 == 0 or
+   (year mod 100 != 0 and
+    year mod 4 == 0)
