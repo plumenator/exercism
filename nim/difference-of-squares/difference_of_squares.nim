071d99556709b9a683fa8966b1703d3de910da23
Karthik Ravikanti <karthik.ravikanti@gmail.com>
2023-06-29 22:20:46 +0800

    Solve nim/difference-of-squares

---
+++ nim/difference-of-squares/difference_of_squares.nim
@@ -0,0 +1,12 @@
+import std/math
+
+proc squareOfSum*(n: int): int =
+  let s = (n * (n + 1)).div(2)
+  s * s
+
+proc sumOfSquares*(n: int): int =
+  for i in 1..n:
+    result += i * i
+
+proc difference*(n: int): int =
+  n.squareOfSum - n.sumOfSquares
