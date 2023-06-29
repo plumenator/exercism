6a0d46494ea29d4cb885f4ecf2bcafcf52a8c36b
Karthik Ravikanti <karthik.ravikanti@gmail.com>
2023-06-29 15:45:20 +0800

    Use foldl

--- nim/hamming/hamming.nim
+++ nim/hamming/hamming.nim
@@ -1,12 +1,6 @@
-import std/[sequtils, sugar, math]
+import std/sequtils
 
 proc distance*(a, b: string): int =
   if a.len != b.len:
     raise newException(ValueError, "different lens")
-  var differences = collect:
-    for (ai, bi) in zip(a, b):
-      if ai == bi:
-        0
-      else:
-        1
-  differences.sum
+  zip(a, b).foldl(a + (if b[0] == b[1]: 0 else: 1), 0)
