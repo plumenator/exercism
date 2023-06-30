52474ba364457649f3517b2b95657cae535f7f5a
Karthik Ravikanti <karthik.ravikanti@gmail.com>
2023-06-30 20:46:32 +0800

    Use inc

--- nim/collatz-conjecture/collatz_conjecture.nim
+++ nim/collatz-conjecture/collatz_conjecture.nim
@@ -3,7 +3,7 @@ proc steps*(n: int): int =
     raise newException(ValueError, "non positive parameter")
   var n = n
   while n > 1:
-    result += 1
+    result.inc
     if n mod 2 == 0:
       n = n div 2
     else:
