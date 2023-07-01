f54b44a627061c761dd4e5dd1f8d86b479f4a2f1
Karthik Ravikanti <karthik.ravikanti@gmail.com>
2023-07-01 21:46:01 +0800

    Use uint64 literal

--- nim/grains/grains.nim
+++ nim/grains/grains.nim
@@ -3,7 +3,7 @@ import std/[sequtils, math]
 proc onSquare*(n: int): uint64 =
   if n < 1 or n > 64:
     raise newException(ValueError, "non positive param")
-  (result + 2) ^ (n - 1)
+  2u64 ^ (n - 1)
 
 proc total*: uint64 =
   # toSeq(1..64).foldl(a + onSquare(b))
