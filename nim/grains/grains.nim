265a33fc9d978ba750337106daf982f253c57902
Karthik Ravikanti <karthik.ravikanti@gmail.com>
2023-07-01 21:50:29 +0800

    Use foldl

--- nim/grains/grains.nim
+++ nim/grains/grains.nim
@@ -6,6 +6,4 @@ proc onSquare*(n: int): uint64 =
   2u64 ^ (n - 1)
 
 proc total*: uint64 =
-  # toSeq(1..64).foldl(a + onSquare(b))
-  for n in 1..64:
-    result += onSquare(n)
+  toSeq(1..64).foldl(a + onSquare(b), 0u64)
