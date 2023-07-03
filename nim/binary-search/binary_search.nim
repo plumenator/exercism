9b4c24c94c8c4e750f43db8c0220f71a99d1071f
Karthik Ravikanti <karthik.ravikanti@gmail.com>
2023-07-03 11:01:07 +0800

    Use iterative algorithm

--- nim/binary-search/binary_search.nim
+++ nim/binary-search/binary_search.nim
@@ -1,20 +1,17 @@
 import std/options
 
-func binarySearchInternal(haystack: openArray[int], needle: int, first, last: int): Option[int] =
-  if first > last:
-    return none(int)
-  if first == last:
-    if needle == haystack[first]:
-      return some(first)
-    else:
-      return none(int)
-  let mid = (first + last) /% 2
-  let left = haystack.binarySearchInternal(needle, mid + 1, last)
-  let right = haystack.binarySearchInternal(needle, first, mid)
-  if left.is_some:
-    left
-  else:
-    right
-
 proc binarySearch*(a: openArray[int], val: int): int =
-  a.binarySearchInternal(val, 0, a.len - 1).get(-1)
+  var
+    first = a.low
+    last = a.high
+
+  while first <= last:
+    let mid = (first + last) div 2
+    if a[mid] < val:
+      first = mid + 1
+    elif a[mid] > val:
+      last = mid - 1
+    else:
+      return mid
+
+  return -1
