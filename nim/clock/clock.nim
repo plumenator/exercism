912a8e4ffdef9324d46b50e1821a09e17cbf5cb3
Karthik Ravikanti <karthik.ravikanti@gmail.com>
2023-07-04 11:00:34 +0800

    Solve nim/clock

---
+++ nim/clock/clock.nim
@@ -0,0 +1,21 @@
+import std/[strformat, math]
+
+type
+  Clock* = object
+    hour*: range[0..23]
+    minute*: range[0..59]
+
+  Minutes* = distinct int
+
+proc initClock*(hour, minute: int): Clock =
+  Clock(hour: (hour + minute.floorDiv(60)).floorMod(24),
+        minute: minute.floorMod(60))
+
+proc `$`*(c: Clock): string =
+  &"{c.hour:02}:{c.minute:02}"
+
+proc `+`*(c: Clock, v: Minutes): Clock =
+  initClock(c.hour.int, c.minute.int + v.int)
+
+proc `-`*(c: Clock, v: Minutes): Clock =
+  initClock(c.hour.int, c.minute.int - v.int)
