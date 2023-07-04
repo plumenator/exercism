c7c64ec8d28a45678371670702a7ca7deaa0b415
Karthik Ravikanti <karthik.ravikanti@gmail.com>
2023-07-04 16:20:10 +0800

    Remove unnecessary return

--- nim/darts/darts.nim
+++ nim/darts/darts.nim
@@ -4,10 +4,10 @@ proc score*(x, y: float): int =
   let distance = sqrt(x ^ 2 + y ^ 2)
 
   if distance <= 1:
-    return 10
+    10
   elif distance <= 5:
-    return 5
+    5
   elif distance <= 10:
-    return 1
+    1
   else:
-    return 0
+    0
