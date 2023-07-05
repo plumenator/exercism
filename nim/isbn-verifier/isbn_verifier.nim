f3f401fe851aa21bb52251562ec23c2d65beab96
Karthik Ravikanti <karthik.ravikanti@gmail.com>
2023-07-05 11:18:31 +0800

    Use countdown

--- nim/isbn-verifier/isbn_verifier.nim
+++ nim/isbn-verifier/isbn_verifier.nim
@@ -6,6 +6,6 @@ proc isValid*(s: string): bool =
   s[0..^2].all(isDigit) and
   (s[^1].isDigit or s[^1].toUpperAscii == 'X') and
   (s.mapIt(if it.isDigit: ord(it) - ord('0') else: 10)
-    .zip(@[10, 9, 8, 7, 6, 5, 4, 3, 2, 1])
+    .zip(countdown(10, 1).toSeq)
     .mapIt(it[0] * it[1])
     .sum mod 11 == 0)
