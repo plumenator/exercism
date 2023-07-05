771de7cf76bf28201f05a0a2e6949b4fc1bdca81
Karthik Ravikanti <karthik.ravikanti@gmail.com>
2023-07-05 11:28:07 +0800

    Use a for loop

--- nim/isbn-verifier/isbn_verifier.nim
+++ nim/isbn-verifier/isbn_verifier.nim
@@ -1,11 +1,24 @@
-import std/[math, strutils, sequtils, sugar]
+import std/[strutils, sequtils]
 
 proc isValid*(s: string): bool =
   let s = s.filter(isAlphaNumeric)
-  s.len == 10 and
-  s[0..^2].all(isDigit) and
-  (s[^1].isDigit or s[^1].toUpperAscii == 'X') and
-  (s.mapIt(if it.isDigit: ord(it) - ord('0') else: 10)
-    .zip(countdown(10, 1).toSeq)
-    .mapIt(it[0] * it[1])
-    .sum mod 11 == 0)
+
+  if s.len != 10:
+    return false
+
+  var sum = 0
+
+  for i, c in s[0..^2]:
+    if c.isDigit:
+      sum += (10 - i) * (ord(c) - ord('0'))
+    else:
+      return false
+
+  if s[^1].toUpperAscii == 'X':
+    sum += 10
+  elif s[^1].isDigit:
+    sum += ord(s[^1]) - ord('0')
+  else:
+    return false
+
+  sum mod 11 == 0
