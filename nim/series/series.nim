148e593390a913d59cbdcd869bfb461c776f73ef
Karthik Ravikanti <karthik.ravikanti@gmail.com>
2023-07-10 21:09:51 +0800

    Solve nim/series

---
+++ nim/series/series.nim
@@ -0,0 +1,11 @@
+import std/sequtils
+
+proc slices*(s: string, n: int): seq[string] =
+  if n == s.len:
+    return @[s]
+  elif n < 1 or n > s.len:
+    raise newException(ValueError, "")
+  result = newSeq[string](s.len - n + 1)
+  for i in 0..<n:
+    result = result.zip(s[i..^1]).mapIt(it[0] & $it[1])
+    
