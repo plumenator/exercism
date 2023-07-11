3a8e6dced64523c9fd8525b65681165d2e4d367d
Karthik Ravikanti <karthik.ravikanti@gmail.com>
2023-07-11 16:35:04 +0800

    Solve nim/yacht

---
+++ nim/yacht/yacht.nim
@@ -0,0 +1,37 @@
+import std/[sequtils, tables, algorithm, math]
+type
+  Category* = enum
+    Ones, Twos, Threes, Fours, Fives, Sixes,
+    FullHouse, FourOfAKind, LittleStraight, BigStraight, Choice, Yacht
+
+proc score*(dice: array[5, int], category: Category): int =
+  let counts = dice.toCountTable
+  case category
+  of Ones, Twos, Threes, Fours, Fives, Sixes:
+    let n = category.int + 1
+    counts[n] * n
+  of FullHouse:
+    if counts.len == 2 and
+       counts.values.toSeq.sorted == @[2, 3]:
+      dice.sum
+    else:
+      0
+  of FourOfAKind:
+    counts.pairs.toSeq.filterIt(it[1] >= 4).mapIt(it[0] * 4).sum
+  of LittleStraight:
+    if dice.sorted == @[1, 2, 3, 4, 5]:
+      30
+    else:
+      0
+  of BigStraight:
+    if dice.sorted == @[2, 3, 4, 5, 6]:
+      30
+    else:
+      0
+  of Choice:
+    dice.sum
+  of Yacht:
+    if counts.len == 1:
+      50
+    else:
+      0
